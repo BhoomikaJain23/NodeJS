773934ec788855e14fa1f1edc6ee72b3849a1c7d
BhoomikaJain23 <jainbhoomika024@gmail.com>
2026-02-22 17:30:36 +0530

    another way to export the function

--- HelloWorld/math.js
+++ HelloWorld/math.js
@@ -10,8 +10,13 @@ function sub(a,b){
 }
 
 
-//object bna lo taki jada function ko extract kr ske
+//object bna lo taki ek se  jada function ko extract kr ske
 module.exports ={
     add,
     sub
 }
+
+
+//another easy way to export the function is
+exports.add=(a,b)=> a+b;
+exports.sub=(a,b)=> a-b;
