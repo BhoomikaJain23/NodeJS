56690270a46528b6dbad860f7c045e5e5e68b7fc
BhoomikaJain23 <jainbhoomika024@gmail.com>
2026-02-22 17:26:51 +0530

    learn about modules

--- HelloWorld/Hello.js
+++ HelloWorld/Hello.js
@@ -1,1 +1,7 @@
 console.log("hey! My name is Bhoomika Jain");
+
+//use this to extract the values of function from math.js
+const math =require("./math") //ab m math.js se uske function extract kr skti hu
+
+console.log("Math value is",math.add(2,3)); //ab m math.js ke add function ko use kr skti hu
+console.log("Math value is",math.sub(2,3)); //ab m math.js ke sub function ko use kr skti hu
---
+++ HelloWorld/math.js
@@ -0,0 +1,17 @@
+
+function add(a,b){
+    return a+b;
+}
+
+
+
+function sub(a,b){
+    return a-b;
+}
+
+
+//object bna lo taki jada function ko extract kr ske
+module.exports ={
+    add,
+    sub
+}
