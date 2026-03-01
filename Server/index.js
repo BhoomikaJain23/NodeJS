a9da46dbf3bcf32b83061b5986657bfff118a1ed
BhoomikaJain23 <jainbhoomika024@gmail.com>
2026-03-01 19:15:48 +0530

    learn about Url

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,33 @@
 const http = require("http");
 const fs= require("fs")
+const url=require("url");
 
 
 const myServer = http.createServer((req,res)=>{
+    if(req.url==='/favicon.ico'){
+        res.end();
+        return;
+    }
     const log=`${Date.now()}:${req.url}New Req Received\n`
+    const myUrl=url.parse(req.url,true);
+    console.log(myUrl);
+    
     fs.appendFile('log.txt', log, (err,data)=>{
-      switch(req.url){
+      switch(req.url.pathname){
+
         case '/':res.end("HomePage");
         break;
-        case '/about':res.end("I am Bhoomika Jain");
+
+        case '/about':
+        const username=myUrl.query.myname;   
+       res.end(`Hi,${username}`)
         break;
+
+        case '/search':
+         const search = myUrl.query.search_query;
+           res.end("Here are your results for " + search)
+           break;
+
         default:
         res.end("404 NotFound");
       }
