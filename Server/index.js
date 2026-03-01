54f3fef01b90fe07099a748a82870b3b15895e65
BhoomikaJain23 <jainbhoomika024@gmail.com>
2026-03-01 19:47:03 +0530

    feat:HTTP Methods

--- Server/index.js
+++ Server/index.js
@@ -8,25 +8,39 @@ const myServer = http.createServer((req,res)=>{
         res.end();
         return;
     }
-    const log=`${Date.now()}:${req.url}New Req Received\n`
+    const log=`${Date.now()}:${req.method} ${req.url} New Req Received\n`
     const myUrl=url.parse(req.url,true);
-    console.log(myUrl);
+    // console.log(myUrl);
     
     fs.appendFile('log.txt', log, (err,data)=>{
-      switch(req.url.pathname){
+        if(err){
+            res.end("Internal Server Error");
+            return;
+        }
 
-        case '/':res.end("HomePage");
+       switch(myUrl.pathname){
+
+        case '/':
+        if(req.method === 'GET')
+        res.end("HomePage");
         break;
 
         case '/about':
         const username=myUrl.query.myname;   
-       res.end(`Hi,${username}`)
+        res.end(`Hi,${username}`)
         break;
 
         case '/search':
-         const search = myUrl.query.search_query;
-           res.end("Here are your results for " + search)
-           break;
+        const search = myUrl.query.search_query;
+        res.end("Here are your results for " + search)
+        break;
+
+        case '/signup' :
+        if(req.method==='GET')
+        res.end("This is a signup form")
+        else if(req.method==="POST")
+            //DB Query
+        res.end("SUCCESS")    
 
         default:
         res.end("404 NotFound");
